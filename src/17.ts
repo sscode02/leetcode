a49a8a790d02e1e74f202891cc68a4e1fdd47212
sscode02 <1227918543@qq.com>
2023-06-16 21:48:58 +0800

    add all

---
+++ src/17.ts
@@ -0,0 +1,23 @@
+function letterCombinations(digits: string): string[] {
+    let firstNum = Number(digits[0])
+
+    const map = [
+        'abc',
+        'def',
+        'ghi',
+        'jkl',
+        'mno',
+        'pqrs',
+        'tuv',
+        'wxyz'
+
+    ]
+
+
+    function backTraking() {
+
+    }
+
+    for (let i = 0; i < map[9].length ) { }
+
+};
